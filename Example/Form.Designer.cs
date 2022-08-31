498c6b6cd2040a73f596e9abd4cb0455b1bfa24c
David V <david.veerman@camcoeng.com.au>
2022-08-31 16:32:09 +0800

    Tweaks to naming and fixed serialisation

--- Example/Form.Designer.cs
+++ Example/Form.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.ganttChart1 = new GanttChart.GanttChart();
+            this.SuspendLayout();
+            // 
+            // ganttChart1
+            // 
+            this.ganttChart1.AutoScroll = true;
+            this.ganttChart1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.ganttChart1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ganttChart1.DayLabelFormats = ((System.Collections.Generic.Dictionary<System.DateTime, string>)(resources.GetObject("ganttChart1.DayLabelFormats")));
+            this.ganttChart1.DefaultDayLabelFormat = "dddd";
+            this.ganttChart1.DefaultTimeLabelFormat = "htt";
+            this.ganttChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ganttChart1.EndDate = new System.DateTime(((long)(0)));
+            this.ganttChart1.EndHourInDay = 24;
+            this.ganttChart1.HeaderBackgroundColor = System.Drawing.Color.LightBlue;
+            this.ganttChart1.Holidays = ((System.Collections.Generic.Dictionary<System.DateTime, string>)(resources.GetObject("ganttChart1.Holidays")));
+            this.ganttChart1.HorizontalGridLinesVisible = false;
+            this.ganttChart1.Location = new System.Drawing.Point(0, 0);
+            this.ganttChart1.MinTimeIntervalWidth = 0;
+            this.ganttChart1.Name = "ganttChart1";
+            this.ganttChart1.NowIndicatorHourOffset = 0;
+            this.ganttChart1.RowIconLocation = GanttChart.Enums.Corner.SW;
+            this.ganttChart1.RowIconSize = new System.Drawing.Size(15, 15);
+            this.ganttChart1.ShowNowIndicator = false;
+            this.ganttChart1.Size = new System.Drawing.Size(800, 450);
+            this.ganttChart1.StartDate = new System.DateTime(((long)(0)));
+            this.ganttChart1.StartHourInDay = 0;
+            this.ganttChart1.TabIndex = 0;
+            this.ganttChart1.TextColor = System.Drawing.Color.Black;
+            this.ganttChart1.TimeLabelFormats = ((System.Collections.Generic.Dictionary<System.DateTime, string>)(resources.GetObject("ganttChart1.TimeLabelFormats")));
+            this.ganttChart1.TopHeaderHeight = 40;
+            this.ganttChart1.VerticalGridLinesVisible = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ganttChart1);
+            this.Name = "Form";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GanttChart.GanttChart ganttChart1;
     }
 }
 
--- Example/Form.cs
+++ Example/Form.cs
@@ -16,27 +16,25 @@ namespace Example
 
         public void InitChart()
         {
-            Chart ganttChart = new Chart(); //Can also be added via the designer
-            ganttChart.StartDate = DateTime.Today;
-            ganttChart.EndDate = DateTime.Today.AddDays(5);
-            ganttChart.StartHourInDay = 8;
-            ganttChart.EndHourInDay = 17;
 
-            this.Controls.Add(ganttChart); //Add the chart to the form
-            ganttChart.Dock = DockStyle.Fill; //Expand the chart to fill the form
+            ganttChart1.StartDate = DateTime.Today;
+            ganttChart1.EndDate = DateTime.Today.AddDays(5);
+            ganttChart1.StartHourInDay = 8;
+            ganttChart1.EndHourInDay = 17;
 
+           
             //Add data
             Row row1 = new Row("Row 1");
             row1.TimeBlocks.Add(new TimeBlock("Shift 1", DateTime.Today.AddHours(8), DateTime.Today.AddHours(13)) { Color = Color.Red });
-            ganttChart.Rows.Add(row1);
+            ganttChart1.Rows.Add(row1);
 
             Row row2 = new Row("Row 2");
             row2.TimeBlocks.Add(new TimeBlock("Shift 1", DateTime.Today.AddHours(8), DateTime.Today.AddHours(10)) { Color = Color.Yellow });
             row2.TimeBlocks.Add(new TimeBlock("Shift 2", DateTime.Today.AddHours(13), DateTime.Today.AddHours(17)) { Color = Color.Purple });
-            ganttChart.Rows.Add(row2);
+            ganttChart1.Rows.Add(row2);
 
 
-            ganttChart.UpdateView();
+            ganttChart1.UpdateView();
         }
     }
 }
